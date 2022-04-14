2f31712d198a82094280d155fea25583310f289b
hello tui <33209790+TUISYS@users.noreply.github.com>
2022-04-14 09:53:55 +0800

    V2.1

--- test.c
+++ test.c
@@ -1,10 +1,10 @@
 #include "tui.h"
 
-static char *tts_string = "��֮�����Ա��ơ��������ϰ��Զ�������̣�����Ǩ����֮��������ר������ĸ�����ڴ����Ӳ�ѧ���ϻ��̡����ɽ�����巽�������ӣ������"
-                    "�����̣���֮�����̲��ϣ�ʦ֮�衣�Ӳ�ѧ�������ˡ��ײ�ѧ���Ϻ�Ϊ�����������������˲�ѧ����֪�塣Ϊ���ӣ�����ʱ����ʦ�ѣ�ϰ���ǡ�"
-                    "����䣬����ϯ��Т���ף�����ִ�������꣬�����档���ڳ�������֪����Т㩣��μ��š�֪ĳ����ʶĳ�ġ�һ��ʮ��ʮ���١��ٶ�ǧ��ǧ����"
-                    "�����ߣ�����ˡ������ߣ������ǡ������ߣ������塣�����ף���˳��Ի���ģ�Ի�ﶬ������ʱ���˲��Ի�ϱ���Ի���������ķ���Ӧ���С�"
-                    "Իˮ��ľ�����������У���������ʮ���ߣ������ʮ��֧����������Ի�Ƶ���������Ի���������Ȩ������£���ů�������л����ڶ�����";
+static char *tts_string = "人之初，性本善。性相近，习相远。苟不教，性乃迁。教之道，贵以专。昔孟母，择邻处。子不学，断机杼。窦燕山，有义方。教五子，名俱扬。"
+                    "养不教，父之过。教不严，师之惰。子不学，非所宜。幼不学，老何为。玉不琢，不成器。人不学，不知义。为人子，方少时。亲师友，习礼仪。"
+                    "香九龄，能温席。孝于亲，所当执。融四岁，能让梨。弟于长，宜先知。首孝悌，次见闻。知某数，识某文。一而十，十而百。百而千，千而万。"
+                    "三才者，天地人。三光者，日月星。三纲者，君臣义。父子亲，夫妇顺。曰春夏，曰秋冬。此四时，运不穷。曰南北，曰西东。此四方，应乎中。"
+                    "曰水火，木金土。此五行，本乎数。十干者，甲至癸。十二支，子至亥。曰黄道，日所躔。曰赤道，当中权。赤道下，温暖极。我中华，在东北。";
 static int tts_cnt = 0;
 static void tui_tts_sound_cb(tui_obj_t *obj, tui_event_e event)
 {
@@ -21,12 +21,12 @@ static void tui_tts_sound_cb(tui_obj_t *obj, tui_event_e event)
 
 void tui_tts_speak_test(void)
 {
-	tui_sound_attri_t attri = { 0 };
+	tui_sound_attri_t attri = { 0 };//注意先清空结构体，避免随机值
 	tui_obj_t * sound;
 
-	/* �������� */
+	/* 创建对象 */
 	sound = tui_sound_create(tui_layer_top());
-	/* �������� */
+	/* 设置属性 */
 	memset(&attri, 0, sizeof(tui_sound_attri_t));
 	attri.cb = tui_tts_sound_cb;
 	tui_sound_set_attri(sound, &attri);
